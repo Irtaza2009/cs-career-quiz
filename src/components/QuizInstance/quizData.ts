afe814314d39dbdc7c1b528ca0a2273fea709182
Irtaza <admin@IRT-MacBook-Pro.local>
2024-06-27 02:26:46 +0500

    Added all the answers and questions for the quiz

---
+++ src/components/QuizInstance/quizData.ts
@@ -0,0 +1,80 @@
+export const quizData = [
+  {
+    questionNumber: 1,
+    questionText: "Which movie genre do you enjoy the most?",
+    answers: {
+      se: "Action 🎬 and adventure 🌍", // for the excitement and thrill.",
+      wd: "Drama 🎭 and comedy 🤣", // for the human stories and humor.",
+      ai: "Science fiction 🚀 and fantasy 🐉", // for the imaginative possibilities.",
+      ds: "Documentary 📽️ and non-fiction 📚", // for the knowledge and insights.",
+      csa: "Mystery 🔍 and thriller 💥", // for the suspense and intrigue.",
+    },
+  },
+  {
+    questionNumber: 2,
+    questionText: "What do you do for fun?",
+    answers: {
+      se: "Go to conventions 🎉",
+      wd: "Explore nature 🌳 and the great outdoors 🏞️",
+      ai: "Read fantasy books 📚 and get lost in stories 📖",
+      ds: "Try new recipes 🍳 and cook up a storm 🌪️",
+      csa: "Do extreme sports like parkour 🏃 or skydiving 🪂",
+    },
+  },
+  {
+    questionNumber: 3,
+    questionText: "If you could have any futuristic gadget, what would it be?",
+    answers: {
+      ai: "A device that can predict the future 🔮",
+      wd: "A smart assistant 🤖 that performs repetitive tasks 🔄",
+      csa: "An invisibility cloak 🧥",
+      se: "A virtual reality headset 🕶️", // that allows you to explore immersive virtual worlds.",
+      ds: "A magical book 📖 that notes down everything happening in my life 📝",
+    },
+  },
+  {
+    questionNumber: 4,
+    questionText: "What would you consider your superpower to be?",
+    answers: {
+      se: "Having the power to shape-shift 🔁",
+      wd: "Possessing the ability to teleport instantly ✨",
+      ds: "Mastering the art of telekinesis 🌀",
+      csa: "Harnessing the power of invisibility 🌌",
+      ai: "Wielding the power of time travel ⏳",
+    },
+  },
+  {
+    questionNumber: 5,
+    questionText: "What's will be your favorite gift from these?",
+    answers: {
+      ds: "A new keyboard ⌨️ with those new clicky keys! ⌨️",
+      ai: "A set of magic trick kits! 🎩",
+      se: "A DIY robot-building kit! 🤖",
+      csa: "A spy kit 🕵️ with hidden gadgets! 🔦",
+      wd: "That new video game 🎮 that just got released! 🕹️",
+    },
+  },
+  {
+    questionNumber: 6,
+    questionText:
+      "If you could live in any fictional universe, which one would you choose?",
+    answers: {
+      csa: "The Matrix 💻",
+      se: "The Star Wars universe 🌌",
+      wd: "The Marvel Universe 🦸",
+      ai: "The Harry Potter universe ⚡🪄",
+      ds: "The world of Sherlock Holmes 🕵️‍♂️",
+    },
+  },
+  {
+    questionNumber: 7,
+    questionText: "If you were to create a new mobile app, what would it be?",
+    answers: {
+      wd: "An educational app 📚", // teaching coding and programming skills with interactive lessons.",
+      ai: "An app that makes all the difficult decisions for me. 🤔", //recommendation app suggesting movies, books, and music based on user preferences.",
+      csa: "A virtual assistant app 📱 that helps users with everyday tasks.",
+      ds: "A fitness app 💪", // tracking workouts and providing personalized training plans.",
+      se: "A social networking app 👥 connecting people with similar interests.",
+    },
+  },
+];
